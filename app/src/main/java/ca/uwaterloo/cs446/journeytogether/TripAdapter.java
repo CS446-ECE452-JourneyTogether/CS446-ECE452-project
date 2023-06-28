6b7e2ea262770af11f5a33634f5e367ffa4fd3aa
Lance <s275xu@uwaterloo.ca>
2023-06-28 14:25:47 -0400

    relationship finish

--- app/src/main/java/ca/uwaterloo/cs446/journeytogether/TripAdapter.java
+++ app/src/main/java/ca/uwaterloo/cs446/journeytogether/TripAdapter.java
@@ -11,6 +11,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
 public class TripAdapter extends RecyclerView.Adapter<TripAdapter.TripViewHolder> {
@@ -69,6 +74,21 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.TripViewHolder
 
         public void bind(Trip trip) {
             this.trip = trip;
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            CollectionReference driverCollection = db.collection("jt_driver");
+            driverCollection.whereEqualTo("id", trip.getDriverId())
+                    .limit(1)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot querySnapshot = task.getResult();
+                            if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                                DocumentSnapshot documentSnapshot = querySnapshot.getDocuments().get(0);
+                                String drivername = documentSnapshot.getString("name");
+                                tripDriverTextView.setText(drivername);
+                            }
+                        }
+                    });
             tripDriverTextView.setText(trip.getDriverId());
             tripDestinationTextView.setText(trip.getDestination());
             tripCostTextView.setText(String.format("$%d", trip.getCost()));
