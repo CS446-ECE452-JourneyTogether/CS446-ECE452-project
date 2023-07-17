aaf96e3ac422a83d0897a83faea15e41e292ba60
Echo-K-Fate <90109320+Echo-K-Fate@users.noreply.github.com>
2023-07-16 23:02:23 -0400

    TripRequest_rough

--- app/src/main/java/ca/uwaterloo/cs446/journeytogether/user/HomeFragment.java
+++ app/src/main/java/ca/uwaterloo/cs446/journeytogether/user/HomeFragment.java
@@ -6,14 +6,45 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 import ca.uwaterloo.cs446.journeytogether.R;
+import ca.uwaterloo.cs446.journeytogether.common.InAppNotice;
+import ca.uwaterloo.cs446.journeytogether.schema.TripRequest;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {}
 
-    @Override
+    private View rootView;
+    private ArrayList<TripRequest> tripRequest = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RequestAdapter RequestAdapter;
+    private InAppNotice inAppNotice;
+    private FirebaseAuth mAuth;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.fragment_home, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_trip_list, container, false);
+        inAppNotice = new InAppNotice(rootView);
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        // placeholder: currently it make query about everything
+        TripRequest.firestore.makeQuery(
+                c -> c.whereEqualTo("Passenger",user.getUid()),
+                (arr) -> {
+                    this.tripRequest = arr;
+                    recyclerView = rootView.findViewById(R.id.tripListRecyclerView);
+                    RequestAdapter = new RequestAdapter(tripRequest, getContext());
+                    recyclerView.setAdapter(RequestAdapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                },
+                () -> { inAppNotice.onError("Cannot fetch the trips. Please try again later."); }
+        );
+        return rootView;
     }
 }
--- app/src/main/java/ca/uwaterloo/cs446/journeytogether/user/RequestAdapter.java
+++ app/src/main/java/ca/uwaterloo/cs446/journeytogether/user/RequestAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,90 +15,75 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
 
 import ca.uwaterloo.cs446.journeytogether.R;
 import ca.uwaterloo.cs446.journeytogether.schema.Trip;
+import ca.uwaterloo.cs446.journeytogether.schema.TripRequest;
 
-public class TripAdapter extends RecyclerView.Adapter<TripAdapter.TripViewHolder> {
+public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.TripViewHolder> {
 
-    private ArrayList<Trip> trips;
+    private ArrayList<TripRequest> tripRequests;
     private Context context;
     private Geocoder geocoder;
 
-    public TripAdapter(ArrayList<Trip> trips, Context context) {
-        this.trips = trips;
+    public RequestAdapter(ArrayList<TripRequest> tripRequest, Context context) {
+        this.tripRequests = tripRequest;
         this.context = context;
     }
 
     @NonNull
     @Override
     public TripViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.trip_item_layout, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.triprequest_item_layout, parent, false);
         return new TripViewHolder(view, context);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TripViewHolder holder, int position) {
-        Trip trip = trips.get(position);
-        holder.bind(trip);
+        TripRequest tripr = tripRequests.get(position);
+        holder.bind(tripr);
     }
 
     @Override
     public int getItemCount() {
-        return trips.size();
+        return tripRequests.size();
     }
 
     public static class TripViewHolder extends RecyclerView.ViewHolder {
         private TextView tripDriverTextView;
         private TextView tripDestinationTextView;
         private TextView tripCostTextView;
-        private TextView tripSeatsLeftTextView;
-        private Button startSendRequestButton;
+        private ImageView StatusImageView;
 //        private ImageView iconImageView;
         private Context context;
-        private Trip trip;
+        private TripRequest tripRequest;
 
         public TripViewHolder(@NonNull View itemView, Context context) {
             super(itemView);
             tripDriverTextView = itemView.findViewById(R.id.tripReqDriverTextView);
             tripDestinationTextView = itemView.findViewById(R.id.tripReqDestinationTextView);
             tripCostTextView = itemView.findViewById(R.id.tripReqCostTextView);
-            tripSeatsLeftTextView = itemView.findViewById(R.id.tripReqSeatsLeftTextView);
-            startSendRequestButton = itemView.findViewById(R.id.startSendRequestButton);
+            StatusImageView = itemView.findViewById(R.id.StatusImageView);
             this.context = context;
-//            iconImageView = itemView.findViewById(R.id.iconImageView);
         }
 
-        // this function enables or disables the Send request button
-        public void setAllowSendRequest(boolean allow) {
-            startSendRequestButton.setVisibility(allow ? View.VISIBLE : View.GONE);
-        }
-
-        public void bind(Trip trip) {
-            this.trip = trip;
+        public void bind(TripRequest tripRequest) {
+            this.tripRequest = tripRequest;
             FirebaseFirestore db = FirebaseFirestore.getInstance();
-            CollectionReference driverCollection = db.collection("jt_driver");
+            CollectionReference driverCollection = db.collection("jt_user");
+            CollectionReference tripCollection = db.collection("jt_trips");
 
+            Trip trip = tripRequest.getTrip();
             if (trip.getDriver() != null) {
                 tripDriverTextView.setText(trip.getDriver().getDisplayName());
             }
 
             tripDestinationTextView.setText(trip.getRouteStringRep(this.context));
             tripCostTextView.setText(String.format("$%d/seat", trip.getCost()));
-            tripSeatsLeftTextView.setText(String.format("%d/%d seats available", trip.getAvailableSeats(), trip.getTotalSeats()));
-//            iconImageView.setImageResource(trip.getIconResId());
 
-            // upon pressing the send button, it takes us to a trip request activity
-            startSendRequestButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(context, TripRequestActivity.class);
-                    intent.putExtra("trip", trip);
-                    context.startActivity(intent);
-                }
-            });
         }
     }
 }
