d4667024249e1dce3620b9fa9335803a1a812708
XinhaoGeUW <x27ge@uwaterloo.ca>
2023-07-19 17:01:40 -0400

    search trip part2

--- app/src/main/java/ca/uwaterloo/cs446/journeytogether/schema/Trip.java
+++ app/src/main/java/ca/uwaterloo/cs446/journeytogether/schema/Trip.java
@@ -2,29 +2,33 @@ package ca.uwaterloo.cs446.journeytogether.schema;
 
 import android.content.Context;
 import android.location.Address;
+import android.location.Geocoder;
 import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.GeoPoint;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import ca.uwaterloo.cs446.journeytogether.common.AddressRep;
 import ca.uwaterloo.cs446.journeytogether.common.DateTimeConverter;
 import ca.uwaterloo.cs446.journeytogether.common.FirestoreCollection;
+import ca.uwaterloo.cs446.journeytogether.common.GeoHashing;
 
 public class Trip implements Serializable {
 
     private String id;
     private User driver;
-    private GeoPoint origin;
-    private GeoPoint destination;
+    private SerializableLatLng origin;
+    private SerializableLatLng destination;
 
     public LocalDateTime getDepartureTime() {
         return departureTime;
@@ -42,65 +46,55 @@ public class Trip implements Serializable {
     private int cost;
     private List<User> passengers;
 
-    private static final String COLLECTION_PATH = "jt_trips_test";
+    private static final String COLLECTION_PATH = "jt_trips";
     public static final FirestoreCollection<Trip> firestore =
             new FirestoreCollection<>(
                     COLLECTION_PATH,
-                    (onto, document) -> {
-                        onto.update(document);
-                    },
+                    (onto, document) -> { onto.update(document); },
                     (value) -> value.asMap(),
-                    (onto, id) -> {
-                        onto.setId(id);
-                    },
+                    (onto, id) -> onto.setId(id),
                     () -> new Trip()
             );
 
     public void update(DocumentSnapshot document) {
         try {
-            String driverId = document.getString("driver");
+            String driverId = (String) document.get("driver");
 
             if (driverId != null) {
                 User.firestore.getValuesById(driverId,
                         (arr) -> {
-                            if (!arr.isEmpty()) {
-                                this.driver = arr.get(0);
-                            }
+                            if (!arr.isEmpty()) { this.driver = arr.get(0); }
                         },
-                        () -> {
-                        }
+                        () -> {}
                 );
             }
 
-            this.origin = document.getGeoPoint("origin");
-            this.destination = document.getGeoPoint("destination");
+            this.origin = new SerializableLatLng(GeoHashing.unhash("origin", document));
+            this.destination = new SerializableLatLng(GeoHashing.unhash("destination", document));
 
-            this.departureTime = DateTimeConverter.toLocalDateTime(document.getTimestamp("departureTime"));
-            this.arrivalTime = DateTimeConverter.toLocalDateTime(document.getTimestamp("arrivalTime"));
+            this.departureTime = DateTimeConverter.toLocalDateTime((Timestamp) document.get("departureTime"));
+            this.arrivalTime = DateTimeConverter.toLocalDateTime((Timestamp) document.get("arrivalTime"));
 
-            this.availableSeats = document.getLong("availableSeats").intValue();
-            this.totalSeats = document.getLong("totalSeats").intValue();
-            this.cost = document.getLong("cost").intValue();
+            this.availableSeats = Math.toIntExact((long) document.get("availableSeats"));
+            this.totalSeats = Math.toIntExact((long) document.get("totalSeats"));
+            this.cost = Math.toIntExact((long) document.get("cost"));
 
             List<String> passengerId = (List<String>) document.get("passengers");
-            this.passengers = new ArrayList<>();
+            this.passengers = new ArrayList<User>();
 
             if (passengerId != null) {
                 for (String id : passengerId) {
                     User.firestore.getValuesById(id,
                             (arr) -> {
-                                if (!arr.isEmpty()) {
-                                    this.passengers.add(arr.get(0));
-                                }
+                                if (!arr.isEmpty()) { this.passengers.add(arr.get(0)); }
                             },
-                            () -> {
-                            }
+                            () -> {}
                     );
                 }
             }
 
-        } catch (Exception e) {
-            Log.e("E", String.format("Error occurred with Trip %s: %s", this.id, e.getMessage()));
+        } catch (ClassCastException e) {
+            Log.e("E", String.format("Casting error occurred with Trip %s: %s", this.id, e.getMessage()));
         }
 
     }
@@ -112,8 +106,8 @@ public class Trip implements Serializable {
             map.put("driver", this.driver.getId());
         }
 
-        map.put("origin", this.origin);
-        map.put("destination", this.destination);
+        GeoHashing.hash("origin", this.origin, map);
+        GeoHashing.hash("destination", this.destination, map);
 
         map.put("departureTime", DateTimeConverter.toTimestamp(departureTime));
         map.put("arrivalTime", DateTimeConverter.toTimestamp(arrivalTime));
@@ -135,17 +129,14 @@ public class Trip implements Serializable {
         this.id = id;
     }
 
-    public String getId() {
-        return this.id;
-    }
+    public String getId() { return this.id; }
 
-    public Trip() {
-    }
+    public Trip() {}
 
     public Trip(User driver, LatLng origin, LatLng destination, int availableSeats, LocalDateTime departureTime, LocalDateTime arrivalTime) {
         this.driver = driver;
-        this.origin = new GeoPoint(origin.latitude, origin.longitude);
-        this.destination = new GeoPoint(destination.latitude, destination.longitude);
+        this.origin = new SerializableLatLng(origin);
+        this.destination = new SerializableLatLng(destination);
         this.availableSeats = availableSeats;
         this.totalSeats = availableSeats;
         this.departureTime = departureTime;
@@ -155,18 +146,26 @@ public class Trip implements Serializable {
     }
 
     public Address getOriginAddress(Context context) {
-        return AddressRep.getLocationAddress(context, new LatLng(origin.getLatitude(), origin.getLongitude()));
+        return AddressRep.getLocationAddress(context, this.origin.toLatLng());
     }
 
     public Address getDestinationAddress(Context context) {
-        return AddressRep.getLocationAddress(context, new LatLng(destination.getLatitude(), destination.getLongitude()));
+        return AddressRep.getLocationAddress(context, this.destination.toLatLng());
     }
 
     public String getRouteStringRep(Context context) {
         return String.format(
                 "%s → %s",
-                AddressRep.getLocationStringAddress(context, new LatLng(origin.getLatitude(), origin.getLongitude())),
-                AddressRep.getLocationStringAddress(context, new LatLng(destination.getLatitude(), destination.getLongitude())));
+                AddressRep.getLocationStringAddress(context, origin.toLatLng()),
+                AddressRep.getLocationStringAddress(context, destination.toLatLng()));
+    }
+
+    public String getOriginLocation(Context context) {
+        return AddressRep.getLocationStringAddress(context, origin.toLatLng());
+    }
+
+    public String getDestinationLocation(Context context) {
+        return AddressRep.getLocationStringAddress(context, destination.toLatLng());
     }
 
     public User getDriver() {
@@ -177,20 +176,20 @@ public class Trip implements Serializable {
         this.driver = driver;
     }
 
-    public GeoPoint getOrigin() {
-        return new GeoPoint(origin.getLatitude(), origin.getLongitude());
+    public LatLng getOrigin() {
+        return origin.toLatLng();
     }
 
     public void setOrigin(LatLng origin) {
-        this.origin = new GeoPoint(origin.latitude, origin.longitude);
+        this.origin = new SerializableLatLng(origin);
     }
 
-    public GeoPoint getDestination() {
-        return new GeoPoint(destination.getLatitude(), destination.getLongitude());
+    public LatLng getDestination() {
+        return destination.toLatLng();
     }
 
     public void setDestination(LatLng destination) {
-        this.destination = new GeoPoint(destination.latitude, destination.longitude);
+        this.destination = new SerializableLatLng(destination);
     }
 
 
--- app/src/main/java/ca/uwaterloo/cs446/journeytogether/user/SearchTripActivity.java
+++ app/src/main/java/ca/uwaterloo/cs446/journeytogether/user/SearchTripActivity.java
@@ -2,29 +2,19 @@ package ca.uwaterloo.cs446.journeytogether.user;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import ca.uwaterloo.cs446.journeytogether.R;
 import ca.uwaterloo.cs446.journeytogether.component.DateTimePickerButton;
 import ca.uwaterloo.cs446.journeytogether.component.LocationPickerButton;
-import ca.uwaterloo.cs446.journeytogether.driver.PostTripActivity;
-import ca.uwaterloo.cs446.journeytogether.schema.Trip;
 
 public class SearchTripActivity extends AppCompatActivity {
 
@@ -73,109 +63,13 @@ public class SearchTripActivity extends AppCompatActivity {
             return;
         }
 
-        GeoPoint userStartLocation = new GeoPoint(origin.latitude, origin.longitude);
-        GeoPoint userEndLocation = new GeoPoint(destination.latitude, destination.longitude);
-        Trip.firestore.makeQuery(
-                c -> c,
-                (arr) -> {
-                    List<Trip> trips = arr;
-                    Collections.sort(trips, (t1, t2) -> {
-                        double originDistance1 = calculateDistance(userStartLocation, t1.getOrigin());
-                        double destinationDistance1 = calculateDistance(userEndLocation, t1.getDestination());
-                        double distanceCost1 = originDistance1 + destinationDistance1;
-
-                        double originDistance2 = calculateDistance(userStartLocation, t2.getOrigin());
-                        double destinationDistance2 = calculateDistance(userEndLocation, t2.getDestination());
-                        double distanceCost2 = originDistance2 + destinationDistance2;
-
-                        return Double.compare(distanceCost1, distanceCost2);
-                    });
-
-                    // Process the sorted trip list
-                    for (Trip trip : trips) {
-                        Log.d("trip111", trip.getOrigin().toString());
-                    }
-
-                },
-                () -> { Toast.makeText(SearchTripActivity.this, "Failed to get trips in search activity. Please try again later.", Toast.LENGTH_LONG).show();; }
-        );
-
-//        db.collection("jt_trips_test")
-//                .get()
-//                .addOnSuccessListener(querySnapshot -> {
-//                    List<Trip> trips = new ArrayList<>();
-//                    for (QueryDocumentSnapshot documentSnapshot : querySnapshot) {
-//                        Trip trip = documentSnapshot.toObject(Trip.class);
-//                        trips.add(trip);
-////                        Log.d("database_trips", trip.toString());
-//                    }
-//
-//                    // Sort trips by ascending distance cost using custom Comparator
-//                    Collections.sort(trips, (t1, t2) -> {
-//                        double originDistance1 = calculateDistance(userStartLocation, t1.getOrigin());
-//                        double destinationDistance1 = calculateDistance(userEndLocation, t1.getDestination());
-//                        double distanceCost1 = originDistance1 + destinationDistance1;
-//
-//                        double originDistance2 = calculateDistance(userStartLocation, t2.getOrigin());
-//                        double destinationDistance2 = calculateDistance(userEndLocation, t2.getDestination());
-//                        double distanceCost2 = originDistance2 + destinationDistance2;
-//
-//                        return Double.compare(distanceCost1, distanceCost2);
-//                    });
-//
-//                    // Process the sorted trip list
-//                    for (Trip trip : trips) {
-//                        System.out.println(trip.toString());
-////                        Log.d("trip", trip.toString());
-//                    }
-//                })
-//                .addOnFailureListener(e -> {
-//                    // Error querying trips
-//                });
-
-//         Query trips based on the selected origin, destination, and departureDateTime
-//        Query searchQuery = db.collection("trips_test")
-//                .whereNearTo("origin", new GeoPoint(origin.latitude, origin.longitude))
-//                .whereNearTo("destination", new GeoPoint(destination.latitude, destination.longitude));
-//
-//        if (departureDateTime != null) {
-//            searchQuery = searchQuery.whereGreaterThan("departureTime", departureDateTime);
-//        }
-//
-//        searchQuery.get().addOnCompleteListener(task -> {
-//            if (task.isSuccessful()) {
-//                List<Trip> matchingTrips = new ArrayList<>();
-//                for (QueryDocumentSnapshot document : task.getResult()) {
-//                    Trip trip = document.toObject(Trip.class);
-//                    matchingTrips.add(trip);
-//                }
-//
-//                // Handle the matchingTrips list (e.g., display in a list view or on the map)
-//            } else {
-//                Toast.makeText(this, "Failed to perform search. Please try again later.", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        // Pass latitude and longitude of the GeoPoint objects to the new activity
+        Intent intent = new Intent(SearchTripActivity.this, SearchedTripsActivity.class);
+        intent.putExtra("originLatitude", origin.latitude);
+        intent.putExtra("originLongitude", origin.longitude);
+        intent.putExtra("destinationLatitude", destination.latitude);
+        intent.putExtra("destinationLongitude", destination.longitude);
+        startActivity(intent);
     }
-
-    private double calculateDistance(GeoPoint point1, GeoPoint point2) {
-        double earthRadius = 6371; // Radius of the Earth in kilometers
-
-        double lat1 = Math.toRadians(point1.getLatitude());
-        double lon1 = Math.toRadians(point1.getLongitude());
-        double lat2 = Math.toRadians(point2.getLatitude());
-        double lon2 = Math.toRadians(point2.getLongitude());
-
-        double dLat = lat2 - lat1;
-        double dLon = lon2 - lon1;
-
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                + Math.cos(lat1) * Math.cos(lat2)
-                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = earthRadius * c;
-
-        return distance;
-    }
-
 }
 
